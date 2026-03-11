d364ed56cd7b618dea6da3de69e912a89953d040
Darshit Rabadiya <darshit.rabadiya@hotmail.com>
2026-03-11 20:28:17 +0530

    Create BaseTest.java as base class for UI automation tests

---
+++ src/test/java/com/example/tests/BaseTest.java
@@ -0,0 +1,15 @@
+package com.example.tests;
+
+public class BaseTest {
+    // Base class for UI automation tests
+
+    // Add common setup and teardown methods here
+    
+    public void setUp() {
+        // Setup code
+    }
+
+    public void tearDown() {
+        // Teardown code
+    }
+}
