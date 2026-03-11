89465b8d299fcdf25f74df16a9d1e9739759c60e
Darshit Rabadiya <darshit.rabadiya@hotmail.com>
2026-03-11 20:28:06 +0530

    Create BasePage class as page object model base class

---
+++ src/test/java/com/example/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.example.pages;
+
+public class BasePage {
+    // Common methods and properties for all page objects
+
+    public void waitForElementVisible(String element) {
+        // Implementation goes here
+    }
+
+    public void click(String element) {
+        // Implementation goes here
+    }
+
+    public String getText(String element) {
+        // Implementation goes here
+        return "";
+    }
+}
